bc5b5a6089dae2ff4fea59d7313d184061d2598d
Mitchell5200 <45913837+Mitchell5200@users.noreply.github.com>
2023-09-14 21:23:57 +0800

    music, add new mods later

---
+++ kubejs/server_scripts/modpack/music_discs.js
@@ -0,0 +1,119 @@
+/*
+///# What are you doing in here?
+Made by Mitchell52 for AllTheMods
+
+*/
+ServerEvents.recipes(e => {
+    //Vanilla
+    e.shaped('minecraft:music_disc_otherside',['MC','SD'],{
+        M: 'mysticalagriculture:blank_record',
+        C: 'mysticalagriculture:creeper_essence',
+        S: 'mysticalagriculture:skeleton_essence',
+        D: 'minecraft:blue_dye'
+    }).id('kubejs:minecraft/music_disk_otherside')
+  
+    //Ars Nouveau
+    e.shaped('ars_nouveau:music_disc_aria_biblio',['MC','SD'],{
+        M: 'mysticalagriculture:blank_record',
+        C: 'ars_nouveau:mendosteen_pod',
+        S: 'ars_nouveau:bombegranate_pod',
+        D: 'ars_nouveau:source_gem'
+    }).id('kubejs:ars_nouveau/music_disc_aria_biblio') 
+/*
+    //botania
+    e.shaped('botania:record_gaia_1',['MC','SD'],{
+        M: 'mysticalagriculture:blank_record',
+        C: 'botania:manasteel_nugget',
+        S: 'mysticalagriculture:mystical_flower_essence',
+        D: 'botania:gray_petal'
+    }).id('kubejs:botania/record_gaia_1')
+    e.shaped('botania:record_gaia_2',['MC','SD'],{
+        M: 'mysticalagriculture:blank_record',
+        C: 'botania:manasteel_nugget',
+        S: 'mysticalagriculture:mystical_flower_essence',
+        D: 'botania:black_petal'
+    }).id('kubejs:botania/record_gaia_2')
+*/
+    //Blue skies
+    e.shaped('blue_skies:blinding_rage',['MC','SD'],{
+        M: 'mysticalagriculture:blank_record',
+        C: 'blue_skies:aquite',
+        S: 'minecraft:gray_dye',
+        D: 'minecraft:blue_dye'
+    }).id('kubejs:blue_skies/blinding_rage')
+    e.shaped('blue_skies:defying_starlight',['MC','SD'],{
+        M: 'mysticalagriculture:blank_record',
+        C: 'blue_skies:aquite',
+        S: 'minecraft:green_dye',
+        D: 'minecraft:yellow_dye'
+    }).id('kubejs:blue_skies/defying_starlight')
+    e.shaped('blue_skies:venomous_encounter',['MC','SD'],{
+        M: 'mysticalagriculture:blank_record',
+        C: 'blue_skies:aquite',
+        S: 'minecraft:magenta_dye',
+        D: 'minecraft:purple_dye'
+    }).id('kubejs:blue_skies/venomous_encounter')
+    e.shaped('blue_skies:population',['MC','SD'],{
+        M: 'mysticalagriculture:blank_record',
+        C: 'blue_skies:aquite',
+        S: 'minecraft:cyan_dye',
+        D: 'minecraft:magenta_dye'
+    }).id('kubejs:blue_skies/population')
+
+    //Twilight
+    e.shaped('twilightforest:music_disc_radiance',['MC','SD'],{
+        M: 'mysticalagriculture:blank_record',
+        C: 'twilightforest:raven_feather',
+        S: 'twilightforest:torchberries',
+        D: 'minecraft:yellow_dye'
+    }).id('kubejs:twilightforest/music_disc_radiance')
+    e.shaped('twilightforest:music_disc_steps',['MC','SD'],{
+        M: 'mysticalagriculture:blank_record',
+        C: 'twilightforest:raven_feather',
+        S: 'twilightforest:torchberries',
+        D: 'minecraft:cyan_dye'
+    }).id('kubejs:twilightforest/music_disc_steps')
+    e.shaped('twilightforest:music_disc_superstitious',['MC','SD'],{
+        M: 'mysticalagriculture:blank_record',
+        C: 'twilightforest:raven_feather',
+        S: 'twilightforest:torchberries',
+        D: 'minecraft:purple_dye'
+    }).id('kubejs:twilightforest/music_disc_superstitious')
+    e.shaped('twilightforest:music_disc_home',['MC','SD'],{
+        M: 'mysticalagriculture:blank_record',
+        C: 'twilightforest:raven_feather',
+        S: 'twilightforest:torchberries',
+        D: 'minecraft:pink_dye'
+    }).id('kubejs:twilightforest/music_disc_home')
+    e.shaped('twilightforest:music_disc_wayfarer',['MC','SD'],{
+        M: 'mysticalagriculture:blank_record',
+        C: 'twilightforest:raven_feather',
+        S: 'twilightforest:torchberries',
+        D: 'minecraft:blue_dye'
+    }).id('kubejs:twilightforest/music_disc_wayfarer')
+    e.shaped('twilightforest:music_disc_findings',['MC','SD'],{
+        M: 'mysticalagriculture:blank_record',
+        C: 'twilightforest:raven_feather',
+        S: 'twilightforest:torchberries',
+        D: 'minecraft:orange_dye'
+    }).id('kubejs:twilightforest/music_disc_findings')
+    e.shaped('twilightforest:music_disc_maker',['MC','SD'],{
+        M: 'mysticalagriculture:blank_record',
+        C: 'twilightforest:raven_feather',
+        S: 'twilightforest:torchberries',
+        D: 'minecraft:magenta_dye'
+    }).id('kubejs:twilightforest/music_disc_maker')
+    e.shaped('twilightforest:music_disc_thread',['MC','SD'],{
+        M: 'mysticalagriculture:blank_record',
+        C: 'twilightforest:raven_feather',
+        S: 'twilightforest:torchberries',
+        D: 'minecraft:red_dye'
+    }).id('kubejs:twilightforest/music_disc_thread')
+    e.shaped('twilightforest:music_disc_motion',['MC','SD'],{
+        M: 'mysticalagriculture:blank_record',
+        C: 'twilightforest:raven_feather',
+        S: 'twilightforest:torchberries',
+        D: 'minecraft:light_blue_dye'
+    }).id('kubejs:twilightforest/music_disc_motion')
+
+})
