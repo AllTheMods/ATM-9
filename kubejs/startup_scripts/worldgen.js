8300e0721778d6592bf2b916ba5a2d38259a0ade
WTD <mattmaryanski@gmail.com>
2023-09-22 21:13:43 -0400

    remove ores

---
+++ kubejs/startup_scripts/worldgen.js
@@ -0,0 +1,20 @@
+WorldgenEvents.remove(event => {
+  event.removeOres(props => {
+    props.worldgenLayer = 'underground_ores';
+    props.blocks = [
+      "railcraft:lead_ore",
+      "railcraft:deepslate_lead_ore",
+      "railcraft:nickel_ore",
+      "railcraft:deepslate_nickel_ore",
+      "railcraft:tin_ore",
+      "railcraft:deepslate_tin_ore",
+      "railcraft:zinc_ore",
+      "railcraft:deepslate_zinc_ore",
+      "create:zinc_ore",
+      "create:deepslate_zinc_ore",
+      "occultism:silver_ore",
+      "occultism:silver_ore_deepslate",
+      "bigreactors:yellorite_ore"
+    ];
+  });
+});
