2aca1f591d01da5431c5bec7290b23ecdc6ab293
Mitchell5200 <45913837+Mitchell5200@users.noreply.github.com>
2023-10-01 11:06:40 +0800

    Update jei_hide.js

--- kubejs/client_scripts/jei_hide.js
+++ kubejs/client_scripts/jei_hide.js
@@ -6,7 +6,7 @@ JEIEvents.hideItems(event => {
   event.hide(['angelring:leadstone_angel_ring', 'angelring:hardened_angel_ring', 'angelring:reinforced_angel_ring', 'angelring:resonant_angel_ring'])
   event.hide('spirit:compressed_soul_sand')
   event.hide('reliquary:rod_of_lyssa')
-  //event.hide('mekanism:upgrade_anchor')
+  event.hide('mekanism:upgrade_anchor')
   event.hide(['mysticalagradditions:gaia_spirit_crux', 'mysticalagradditions:awakened_draconium_crux'])
   event.hide('ars_nouveau:glyph_animate_block')
   event.hide([
