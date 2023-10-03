33bc2f2676c98a4331539dd336723b527804b2a0
Alex Loving <D.Alex.Loving@gmail.com>
2023-10-03 11:23:02 -0400

    Clean up comments

--- kubejs/client_scripts/observeGT.js
+++ kubejs/client_scripts/observeGT.js
@@ -1,17 +1,10 @@
 const mapGTMachineIdToTaskId = {
-    /* Replace with block id -> task id mapping */
 	"gtceu:electric_blast_furnace": "3F5D1730023562C7",
 	"gtceu:cleanroom": "3DA6D564BBFB1F50"
 }
 
-/*const mapGTFluidToTaskId = {
-    // fluid id -> task id mapping
-	"gtceu:rubber": "4B25F32D88057B65"
-}*/
-
 const $MetaMachine = Java.loadClass('com.gregtechceu.gtceu.api.blockentity.MetaMachineBlockEntity')
 const $MultiController = Java.loadClass('com.gregtechceu.gtceu.api.machine.feature.multiblock.IMultiController')
-//const $WorkableMachine = Java.loadClass('com.gregtechceu.gtceu.api.machine.WorkableTieredMachine')
 const $CompoundTag = Java.loadClass('net.minecraft.nbt.CompoundTag')
 
 ClientEvents.tick(event => {
@@ -32,34 +25,7 @@ ClientEvents.tick(event => {
                         }
                     }
                 }
-				/*
-				// None of the following works for checking what fluid is in a gtceu machine because that data is not synced with the client
-				// All attempts return minecraft:empty
-                // Capability machines
-                else if (blockEntity.metaMachine instanceof $WorkableMachine) {
-					//console.info(JSON.stringify(blockEntity.compoundTag))
-					console.info(JSON.stringify(block.entityData))
-					console.info(block.entityData.toString())
-                    // let tanks = blockEntity.metaMachine.importFluids.storages;
-					let tanks = blockEntity.metaMachine.exportFluids.storages;
-                    for (let i = 0; i < tanks.length; ++i) {
-                        let tank = tanks[i]
-						//console.info(tank.fluid + ' is tank.fluid')
-						//console.info(tank.fluid.getFluid() + ' is tank.fluid.getFluid')
-						//console.info(tank.fluid.getFluid().fluidName + ' is tank.fluid.getFluid.name')
-                        let fluid = tank.fluid.rawFluid.builtInRegistryHolder().key().location()
-                        let taskString = mapGTFluidToTaskId[fluid.toString()]
-						//console.info(fluid.toString() + ' is fluid ' + i + ', corresponding to ' + taskString)
-						//console.info(tank.fluid.rawFluid.toString() + ' is tank.fluid.rawFluid')
-                        if (taskString) {
-                            let tag = new $CompoundTag()
-                            tag.putString('task', taskString)
-                            event.player.sendData('customTask', tag)
-                        }
-                    }
-                } */
             }
-
         }
     }
 })
