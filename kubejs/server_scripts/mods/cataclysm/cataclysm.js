937d6e139b1bd9bc35661f097105f358b8965bf1
Alex Loving <D.Alex.Loving@gmail.com>
2023-10-20 14:14:07 -0400

    Add heart of sea drop to Deepling Brute and The Leviathan

---
+++ kubejs/server_scripts/mods/cataclysm/cataclysm.js
@@ -0,0 +1,12 @@
+ServerEvents.entityLootTables(event => {
+    event.modifyEntity('cataclysm:deepling_brute', table => {
+        table.addPool(pool => {
+            pool.addItem('minecraft:heart_of_the_sea').randomChanceWithLooting(0.005, 0.005).killedByPlayer()
+        })
+    })
+    event.modifyEntity('cataclysm:the_leviathan', table => {
+        table.addPool(pool => {
+            pool.addItem('minecraft:heart_of_the_sea').killedByPlayer()
+        })
+    })
+})
