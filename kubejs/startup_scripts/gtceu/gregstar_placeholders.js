07bd01815be01a4e706fc90ecad5903ed533445d
Alex Loving <D.Alex.Loving@gmail.com>
2023-11-02 22:46:49 -0400

    Add gregstar shards and some initial uses for the gregstar

--- kubejs/startup_scripts/gtceu/gregstar_placeholders.js
+++ kubejs/startup_scripts/gtceu/gregstar_placeholders.js
@@ -4,4 +4,5 @@ StartupEvents.registry('item', event => {
     event.create('star_compression_module').displayName('Star Compression Module')
     event.create('superthermal_transference_coil').displayName('Superthermal Transference Coil')
     event.create('cable_of_hyperconductivity').displayName('Cable of Hyperconductivity')
+    event.create('greg_star_shard').displayName('Greg Star Shard').glow(true)
 })
