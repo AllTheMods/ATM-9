3ad959aa7716b84fe1e2695d45bcb938a3e73da5
screret <68943070+screret@users.noreply.github.com>
2023-11-07 11:14:42 +0200

    fix mega fusion reactor pattern

--- kubejs/client_scripts/jei_hide.js
+++ kubejs/client_scripts/jei_hide.js
@@ -14,6 +14,10 @@ let cropRemove = [
     "graphite",
     "rubber",
     "grains_of_infinity",
+		"soularium",
+		"dark_steel",
+		"pulsating_alloy",
+		"energetic_alloy",
 ]
 
 let chemlibRemove = [
