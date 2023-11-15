0683ecb7801ac9fb1a7a8b975eb5df9062349ad5
Alex Loving <xurian28@gmail.com>
2023-11-15 11:41:23 -0500

    Add fission core recipe for chemlib

--- kubejs/server_scripts/mods/chemlib/craftRemove.js
+++ kubejs/server_scripts/mods/chemlib/craftRemove.js
@@ -41,6 +41,46 @@ ServerEvents.recipes(event => {
         event.remove({id:`alchemistry:compactor/${material}_dust`})
     })
 
+    // adjust recipe for fusion core to not use tungsten ingots
     event.remove({id: "alchemistry:fusion_core"})
     event.shaped('alchemistry:fusion_core', ['TNT','TNT','TNT',], {'T': 'chemlib:potassium_ingot', 'N': 'minecraft:netherite_scrap'}).id('kubejs:chemlib/fusion_core')
+
+    //adjust recipe for fission core to not use yttrium ingots
+    event.remove({id: "alchemistry:fission_core"})
+    event.shaped('alchemistry:fission_core',
+        ['ABA', 'ABA', 'ABA'],
+        {'A': 'chemlib:zirconium_ingot', 'B': 'minecraft:blaze_rod'}
+    ).id('kubejs:chemlib/fission_core')
+
+    // add a dissolver recipe for rutile dust to make the titanium element
+    // this allows the titanium bee to produce chemlib titanium which can be used for nether star production in chemlib
+    event.custom({
+        "type": "alchemistry:dissolver",
+        "group": "alchemistry:dissolver",
+        "input": {
+            "count": 1,
+            "ingredient": {
+              "item": "gtceu:rutile_dust"
+            }
+        },
+        "output": {
+            "groups": [
+                {
+                    "probability": 100.0,
+                    "results": [
+                        {
+                            "count": 8,
+                            "item": "chemlib:titanium"
+                        },
+                        {
+                            "count": 16,
+                            "item": "chemlib:oxygen"
+                        }
+                    ]
+                }
+            ],
+            "rolls": 1,
+            "weighted": false
+        }
+    })
 })
