5171aed476d33f5b2ef98f1209ab45fac8414728
Alex Loving <xurian28@gmail.com>
2023-12-11 13:45:23 -0500

    add the allthemodium furnaces into a creative tab

--- kubejs/startup_scripts/custom_additions.js
+++ kubejs/startup_scripts/custom_additions.js
@@ -5,3 +5,12 @@ StartupEvents.registry('block', event => {
 StartupEvents.registry('item', event => {
 	event.create('inert_nether_star').displayName('Inert Nether Star').texture(`minecraft:item/nether_star`).tooltip('Needs activating...')//.parentModel('minecraft:nether_star')
 })
+
+StartupEvents.modifyCreativeTab('ironfurnaces:ironfurnaces_tab', event => {
+	event.add('ironfurnaces:allthemodium_furnace')
+	event.add('ironfurnaces:vibranium_furnace')
+	event.add('ironfurnaces:unobtainium_furnace')
+	event.add('ironfurnaces:upgrade_allthemodium')
+	event.add('ironfurnaces:upgrade_vibranium')
+	event.add('ironfurnaces:upgrade_unobtainium')
+})
