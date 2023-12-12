c8239c6be3cf4ea9098cbd4665888c21a9f9f0f8
Alex Loving <xurian28@gmail.com>
2023-12-12 16:48:36 -0500

    change some weights and add more meteors

--- kubejs/server_scripts/mods/bloodmagic/meteors.js
+++ kubejs/server_scripts/mods/bloodmagic/meteors.js
@@ -55,7 +55,7 @@ ServerEvents.highPriorityData(event => {
         event.addJson(`bloodmagic:recipes/meteor/${id}`, meteorJson)
     }
 
-    let endMap = [
+    const endMap = [
         {
             "block": "gtceu:endstone_naquadah_ore",
             "weight": 200
@@ -70,22 +70,22 @@ ServerEvents.highPriorityData(event => {
         }
     ]
 
-    let netherMap = [
+    const netherMap = [
         {
             "block": "gtceu:netherrack_sphalerite_ore",
             "weight": 200
         },
         {
             "block": "gtceu:netherrack_sulfur_ore",
-            "weight": 400
+            "weight": 300
         },
         {
             "block": "gtceu:netherrack_tetrahedrite_ore",
-            "weight": 400
+            "weight": 300
         }
     ]
 
-    let overworldMap = [
+    const overworldMap = [
         {
             "block": "gtceu:tantalite_ore",
             "weight": 200
@@ -100,7 +100,7 @@ ServerEvents.highPriorityData(event => {
         }
     ]
     
-    let speedRuneMap = [
+    const speedRuneMap = [
         {
             "block": "bloodmagic:dungeon_ore",
             "weight": 400
@@ -111,7 +111,7 @@ ServerEvents.highPriorityData(event => {
         }
     ]
 
-    let atmMap = [
+    const atmMap = [
         {
             "block": "allthemodium:unobtainium_ore",
             "weight": 50
@@ -126,9 +126,61 @@ ServerEvents.highPriorityData(event => {
         }
     ]
 
+    const diamondMap = [
+        {
+            "block": "minecraft:diamond_ore",
+            "weight": 200
+        },
+        {
+            "block": "minecraft:emerald_ore",
+            "weight": 200
+        },
+        {
+            "block": "minecraft:coal_ore",
+            "weight": 400
+        }
+    ]
+
+    const netherstarMap = [
+        {
+            "block": "minecraft:wither_skeleton_skull",
+            "weight": 100
+        },
+        {
+            "block": "allthetweaks:nether_star_block",
+            "weight": 50
+        }
+    ]
+
+    const palladiumMap = [
+        {
+            "block": "gtceu:endstone_chromite_ore",
+            "weight": 300
+        },
+        {
+            "block": "gtceu:endstone_bauxite_ore",
+            "weight": 300
+        },
+        {
+            "block": "gtceu:endstone_palladium_ore",
+            "weight": 200
+        }
+    ]
+
     addMeteorWithCore('intricate_parts', 12, { "item": "bloodmagic:hellforgedparts" }, "bloodmagic:dungeon_ore", 1, 3, "bloodmagic:dungeon_stone", speedRuneMap, 5000000)
-    addMeteor('endgtores', 6, { "item": "gtceu:naquadah_ingot" }, 3, "minecraft:endstone", endMap, 2500000)
+    addMeteor('endgtores', 6, { "item": "gtceu:naquadah_ingot" }, 3, "minecraft:end_stone", endMap, 2500000)
     addMeteor('nethergtores', 8, { "item": "gtceu:titanium_ingot" }, 4, "minecraft:netherrack", netherMap, 1250000)
     addMeteor('overworldgtores', 8, { "item": "gtceu:arsenic_dust" }, 4, "minecraft:cobblestone", overworldMap, 625000)
     addMeteor('allthemodium', 12, { "item": "allthemodium:unobtainium_ingot" }, 3, "allthemodium:ancient_stone", atmMap, 2500000)
+
+    addMeteorWithCore('diamond_block', 16, { "item": "allthecompressed:diamond_block_1x" }, "minecraft:diamond_block", 2, 8, "minecraft:stone", diamondMap, 1000000)
+    addMeteorWithCore('netherstar', 24, { "item": "allthecompressed:nether_star_block_1x" }, "allthetweaks:nether_star_block", 2, 5, "minecraft:soul_sand", netherstarMap, 7500000)
+    addMeteor('palladium', 6, { "item": "gtceu:palladium_ingot" }, 3, "minecraft:end_stone", palladiumMap, 2500000)
+
+    addMeteorWithCore('intricate_parts_big', 24, { "item": "allthetweaks:withers_compass" }, "bloodmagic:dungeon_ore", 4, 6, "bloodmagic:dungeon_stone", speedRuneMap, 9750000)
+    addMeteor('endgtores_big', 12, { "item": "gtceu:uranium_rhodium_dinaquadide_block" }, 6, "minecraft:end_stone", endMap, 500)
+    addMeteor('nethergtores_big', 16, { "item": "gtceu:indium_tin_barium_titanium_cuprate_block" }, 8, "minecraft:netherrack", netherMap, 250)
+    addMeteor('overworldgtores_big', 16, { "item": "gtceu:samarium_iron_arsenic_oxide_block" }, 8, "minecraft:cobblestone", overworldMap, 125)
+    addMeteor('allthemodium_big', 12, { "item": "allthemodium:unobtainium_vibranium_alloy_block" }, 6, "allthemodium:ancient_stone", atmMap, 500)
+    addMeteor('palladium_big', 12, { "item": "gtceu:rhodium_plated_palladium_block" }, 6, "minecraft:end_stone", palladiumMap, 500)
 })
