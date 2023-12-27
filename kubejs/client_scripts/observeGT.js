acfabab329bf53fff20cacebefdf693972520555
thevortex <james.g.bowley@gmail.com>
2023-12-26 19:45:58 -0700

    misc fixes

--- kubejs/client_scripts/observeGT.js
+++ kubejs/client_scripts/observeGT.js
@@ -37,11 +37,11 @@ ClientEvents.tick(event => {
 })
 
 NetworkEvents.dataReceived('customTask', event => {
-    const {entity, data, level} = event
-    let taskString = data.task
-    let task = FTBQuests.getObject(level, taskString)
-    let playerQuestData = FTBQuests.getData(entity)
-    if (task && playerQuestData && !playerQuestData.isCompleted(task) && playerQuestData.canStartTasks(task.quest)) {
-        playerQuestData.addProgress(task, 1)
-    }
+    //const {entity, data, level} = event
+    //let taskString = data.task
+    //let task = FTBQuests.getObject(level, taskString)
+    //let playerQuestData = FTBQuests.getData(entity)
+    //if (task && playerQuestData && !playerQuestData.isCompleted(task) && playerQuestData.canStartTasks(task.quest)) {
+    //    playerQuestData.addProgress(task, 1)
+    //}
 })
