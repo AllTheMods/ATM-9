fba3a940759560a48910295807bfce50a2525d29
GizmoTheMoonPig <67468252+GizmoTheMoonPig@users.noreply.github.com>
2024-01-23 21:08:58 -0700

    properly disable the uncrafting table

--- kubejs/client_scripts/jei_hide.js
+++ kubejs/client_scripts/jei_hide.js
@@ -2,7 +2,6 @@ JEIEvents.hideItems(event => {
   event.hide(/extrastorage:(block|disk|storagepart)_.+/)
   event.hide(/rebornstorage:(small|medium|large|larger)_(item|fluid)_disk.*/)
   event.hide(/extrastorage:advanced_(importer|exporter)/)
-  event.hide('twilightforest:uncrafting_table')
   event.hide(['angelring:leadstone_angel_ring', 'angelring:hardened_angel_ring', 'angelring:reinforced_angel_ring', 'angelring:resonant_angel_ring'])
   event.hide('spirit:compressed_soul_sand')
   event.hide('reliquary:rod_of_lyssa')
