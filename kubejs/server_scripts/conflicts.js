e8a9196d47d0d82cdf2cfe442ed129f1ee0bf8d0
whatthedrunk <mattmaryanski@gmail.com>
2024-02-02 20:06:16 -0500

    Greg gem changes

--- kubejs/server_scripts/conflicts.js
+++ kubejs/server_scripts/conflicts.js
@@ -8,12 +8,6 @@ ServerEvents.recipes(event => {
   event.remove({ id: 'additional_lights:fire_for_standing_torch_s' })
   event.shaped('additional_lights:fire_for_standing_torch_s', ['S', 'C'], { S: '#forge:rods/wooden', C: '#minecraft:coals' })
 
-  // GT Flawed Gems
-  event.remove({ id: 'gtceu:shapeless/gem_to_gem_flawedgem_coal' })
-  event.shaped('2x gtceu:coal_flawed_gem', ['CC'], { C: 'minecraft:coal' })
-  event.remove({ id: 'gtceu:shapeless/gem_to_gem_flawedgem_lapis' })
-  event.shaped('2x gtceu:lapis_flawed_gem', ['LL'], { L: 'minecraft:lapis_lazuli' })
-
   // GT Duplicate Removals
   event.remove({ id: 'gtceu:shaped/block_compress_nether_star' })
   event.remove({ id: 'gtceu:shaped/block_compress_ender_pearl' })
