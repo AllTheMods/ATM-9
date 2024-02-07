d419f35d96e34dd420b16777f4529b9662090ee2
Kevin-Marsh <Kevin-Marsh@users.noreply.github.com>
2024-02-07 18:30:14 +1000

    Add oil Compatibility to new Ad Astra

--- kubejs/server_scripts/mods/ad_astra/ad_astra.js
+++ kubejs/server_scripts/mods/ad_astra/ad_astra.js
@@ -1,11 +1,20 @@
 ServerEvents.recipes(e=> {
     e.remove({ id: 'ad_astra:recipes/steel_ingot_from_blasting_iron_ingot'})
-  
+
     //oil is oil
     e.custom({
-        type: "ad_astra:fuel_conversion",
-        input: "#forge:crude_oil",
-        output: "ad_astra:fuel",
-        conversion_ratio: 1.0,
+        type: "ad_astra:refining",
+        cookingtime: 1,
+        energy: 30,
+        input: {
+            ingredient: {
+                tag: "forge:crude_oil"
+              },
+            millibuckets: 5
+        },
+        result: {
+            fluid: "ad_astra:fuel",
+            millibuckets: 5
+        }
     })
 })
