ba3b688b5b9e5197902ee72109405a7733d34997
Kevin-Marsh <Kevin-Marsh@users.noreply.github.com>
2024-02-21 09:40:13 +1000

    Implement a universal press for AE

--- kubejs/server_scripts/mods/ae/recipes.js
+++ kubejs/server_scripts/mods/ae/recipes.js
@@ -7,3 +7,14 @@ ServerEvents.recipes(event => {
     N: 'minecraft:netherite_ingot'
   }).id('kubejs:aeinfinitybooster/infinity_card')
 })
+
+ServerEvents.recipes(event => {
+  event.shaped('kubejs:universal_press', ['FPF', 'CSL', 'FEF'], {
+    F: '#forge:ingots/sky_steel',
+    P: 'ae2:silicon_press',
+    C: 'ae2:calculation_processor_press',
+    S: 'minecraft:slime_ball',
+    L: 'ae2:logic_processor_press',
+    E: 'ae2:engineering_processor_press'
+  }).id('kubejs:universal_press')
+ })
---
+++ kubejs/startup_scripts/AE2/Universal_Press.js
@@ -0,0 +1,7 @@
+StartupEvents.registry('item', event => {
+    event
+    .create('universal_press')
+    .texture('kubejs:item/universal_press')
+    .maxStackSize(64)
+    .displayName('Universal Press');  
+})
