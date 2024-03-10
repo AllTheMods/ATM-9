63240cd9dca4b42726f2709e91000becdbb6e932
Mitchell5200 <45913837+Mitchell5200@users.noreply.github.com>
2024-03-10 22:09:00 +0800

    Update jei_hide.js
    fix extra })

--- kubejs/client_scripts/jei_hide.js
+++ kubejs/client_scripts/jei_hide.js
@@ -21,7 +21,7 @@ JEIEvents.hideItems(event => {
     event.add('extradisks:infinite_fluid_storage_disk', [Text.of('Recipe disabled').red()])
     event.add('extradisks:infinite_fluid_storage_block', [Text.of('Recipe disabled').red()])
     event.add('extradisks:infinite_fluid_storage_part', [Text.of('Recipe disabled').red()])
-})
+
   Color.DYE.forEach(color => {
     ['controller', 'creative_controller', 'grid', 'crafting_grid', 'pattern_grid', 'fluid_grid', 'network_receiver', 'network_transmitter', 'relay', 'detector', 'security_manager', 'wireless_transmitter', 'disk_manipulator', 'crafter', 'crafter_manager', 'crafting_monitor'].forEach(machine => {
       event.hide(`refinedstorage:${color}_${machine}`)
