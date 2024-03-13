df0111737e3007c1c65ea05147ba127376626b23
Mitchell5200 <45913837+Mitchell5200@users.noreply.github.com>
2024-03-13 20:42:59 +0800

    Update atm_alloys.js
    Alloys in Botania, and alloy blocks

--- kubejs/server_scripts/modpack/atm_alloys.js
+++ kubejs/server_scripts/modpack/atm_alloys.js
@@ -116,128 +116,258 @@ event.custom({
 
 }).id(`kubejs:awakening/awakened_unobtainium_vibranium_alloy_block`)
 
-/*
 //Botania
 //
 //allthemodium-vibranium ingot
-  event.custom({
-    "type": "botania:terra_plate",
-    "ingredients": [
-      {
-        "item": "allthemodium:allthemodium_ingot"
-      },
-      {
-        "item": "allthemodium:piglich_heart"
-      },
-      {
-       "item": "allthemodium:vibranium_ingot"
-      }
-    ],
-    "mana": 1000000,
-    "result": {
-      "item": "allthemodium:vibranium_allthemodium_alloy_ingot"
+event.custom({
+  "type": "botania:terra_plate",
+  "ingredients": [
+    {
+      "item": "allthemodium:allthemodium_ingot"
+    },
+    {
+      "item": "allthemodium:piglich_heart"
+    },
+    {
+     "item": "allthemodium:vibranium_ingot"
     }
-  }).id(`kubejs:terra/allthemodium_vibranium_allthemodium_alloy_ingot`)
+  ],
+  "mana": 1000000,
+  "result": {
+    "item": "allthemodium:vibranium_allthemodium_alloy_ingot"
+  }
+}).id(`kubejs:terra/allthemodium_vibranium_allthemodium_alloy_ingot`)
 
 //allthemodium-unobtainium ingot
-  event.custom({
-    "type": "botania:terra_plate",
-    "ingredients": [
-      {
-        "item": "allthemodium:allthemodium_ingot"
-      },
-      {
-        "item": "allthemodium:piglich_heart"
-      },
-      {
-       "item": "allthemodium:unobtainium_ingot"
-      }
-    ],
-    "mana": 1000000,
-    "result": {
-      "item": "allthemodium:unobtainium_allthemodium_alloy_ingot"
+event.custom({
+  "type": "botania:terra_plate",
+  "ingredients": [
+    {
+      "item": "allthemodium:allthemodium_ingot"
+    },
+    {
+      "item": "allthemodium:piglich_heart"
+    },
+    {
+     "item": "allthemodium:unobtainium_ingot"
     }
-  }).id(`kubejs:terra/allthemodium_unobtainium_allthemodium_alloy_ingot`)
+  ],
+  "mana": 1000000,
+  "result": {
+    "item": "allthemodium:unobtainium_allthemodium_alloy_ingot"
+  }
+}).id(`kubejs:terra/allthemodium_unobtainium_allthemodium_alloy_ingot`)
 
 //unobtainium-vibranium ingot
-  event.custom({
-    "type": "botania:terra_plate",
-    "ingredients": [
-      {
-        "item": "allthemodium:unobtainium_ingot"
-      },
-      {
-        "item": "allthemodium:piglich_heart"
-      },
-      {
-       "item": "allthemodium:vibranium_ingot"
-      }
-    ],
-    "mana": 1000000,
-    "result": {
-      "item": "allthemodium:unobtainium_vibranium_alloy_ingot"
+event.custom({
+  "type": "botania:terra_plate",
+  "ingredients": [
+    {
+      "item": "allthemodium:unobtainium_ingot"
+    },
+    {
+      "item": "allthemodium:piglich_heart"
+    },
+    {
+     "item": "allthemodium:vibranium_ingot"
     }
-  }).id(`kubejs:terra/allthemodium_unobtainium_vibranium_alloy_ingot`)
+  ],
+  "mana": 1000000,
+  "result": {
+    "item": "allthemodium:unobtainium_vibranium_alloy_ingot"
+  }
+}).id(`kubejs:terra/allthemodium_unobtainium_vibranium_alloy_ingot`)
 
 //allthemodium-vibranium block
-  event.custom({
-    "type": "botania:terra_plate",
-    "ingredients": [
-      {
-        "item": "allthemodium:allthemodium_block"
-      },
-      {
-        "item": "kubejs:piglich_heart_block"
-      },
-      {
-       "item": "allthemodium:vibranium_block"
-      }
-    ],
-    "mana": 8100000,
-    "result": {
-      "item": "allthemodium:vibranium_allthemodium_alloy_block"
+event.custom({
+  "type": "botania:terra_plate",
+  "ingredients": [
+    {
+      "item": "allthemodium:allthemodium_block"
+    },
+    {
+      "item": "allthemodium:piglich_heart_block"
+    },
+    {
+     "item": "allthemodium:vibranium_block"
     }
-  }).id(`kubejs:terra/allthemodium_vibranium_allthemodium_alloy_block`)
+  ],
+  "mana": 8100000,
+  "result": {
+    "item": "allthemodium:vibranium_allthemodium_alloy_block"
+  }
+}).id(`kubejs:terra/allthemodium_vibranium_allthemodium_alloy_block`)
 
 //allthemodium-unobtainium block
-  event.custom({
-    "type": "botania:terra_plate",
-    "ingredients": [
-      {
-        "item": "allthemodium:allthemodium_block"
-      },
-      {
-        "item": "kubejs:piglich_heart_block"
-      },
-      {
-       "item": "allthemodium:unobtainium_block"
-      }
-    ],
-    "mana": 8100000,
-    "result": {
-      "item": "allthemodium:unobtainium_allthemodium_alloy_block"
+event.custom({
+  "type": "botania:terra_plate",
+  "ingredients": [
+    {
+      "item": "allthemodium:allthemodium_block"
+    },
+    {
+      "item": "allthemodium:piglich_heart_block"
+    },
+    {
+     "item": "allthemodium:unobtainium_block"
     }
-  }).id(`kubejs:terra/allthemodium_unobtainium_allthemodium_alloy_block`)
+  ],
+  "mana": 8100000,
+  "result": {
+    "item": "allthemodium:unobtainium_allthemodium_alloy_block"
+  }
+}).id(`kubejs:terra/allthemodium_unobtainium_allthemodium_alloy_block`)
 
 //unobtainium-vibranium block
-  event.custom({
-    "type": "botania:terra_plate",
-    "ingredients": [
-      {
-        "item": "allthemodium:unobtainium_block"
-      },
-      {
-        "item": "kubejs:piglich_heart_block"
-      },
-      {
-       "item": "allthemodium:vibranium_block"
-      }
-    ],
-    "mana": 8100000,
-    "result": {
-      "item": "allthemodium:unobtainium_vibranium_alloy_block"
+event.custom({
+  "type": "botania:terra_plate",
+  "ingredients": [
+    {
+      "item": "allthemodium:unobtainium_block"
+    },
+    {
+      "item": "allthemodium:piglich_heart_block"
+    },
+    {
+     "item": "allthemodium:vibranium_block"
     }
-  }).id(`kubejs:terra/allthemodium_unobtainium_vibranium_alloy_block`)
+  ],
+  "mana": 8100000,
+  "result": {
+    "item": "allthemodium:unobtainium_vibranium_alloy_block"
+  }
+}).id(`kubejs:terra/allthemodium_unobtainium_vibranium_alloy_block`)
 
-*/
+//Mythic Botany
+//allthemodium-vibranium ingot
+event.custom({
+  "type": "mythicbotany:infuser",
+  "fromColor": 16750080,
+  "ingredients": [
+    {
+      "item": "allthemodium:allthemodium_ingot"
+    },
+    {
+      "item": "allthemodium:piglich_heart"
+    },
+    {
+     "item": "allthemodium:vibranium_ingot"
+    }
+  ],
+  "mana": 1000000,
+  "output": {
+    "item": "allthemodium:vibranium_allthemodium_alloy_ingot"
+  },
+  "toColor": 4565073
+}).id(`kubejs:infuser/allthemodium_vibranium_allthemodium_alloy_ingot`)
+
+//allthemodium-unobtainium ingot
+event.custom({
+  "type": "mythicbotany:infuser",
+  "fromColor": 16750080,
+  "ingredients": [
+    {
+      "item": "allthemodium:allthemodium_ingot"
+    },
+    {
+      "item": "allthemodium:piglich_heart"
+    },
+    {
+     "item": "allthemodium:unobtainium_ingot"
+    }
+  ],
+  "mana": 1000000,
+  "output": {
+    "item": "allthemodium:unobtainium_allthemodium_alloy_ingot"
+  },
+  "toColor": 6292164
+}).id(`kubejs:infuser/allthemodium_unobtainium_allthemodium_alloy_ingot`)
+
+//unobtainium-vibranium ingot
+event.custom({
+  "type": "mythicbotany:infuser",
+  "fromColor": 6292164,
+  "ingredients": [
+    {
+      "item": "allthemodium:unobtainium_ingot"
+    },
+    {
+      "item": "allthemodium:piglich_heart"
+    },
+    {
+     "item": "allthemodium:vibranium_ingot"
+    }
+  ],
+  "mana": 1000000,
+  "output": {
+    "item": "allthemodium:unobtainium_vibranium_alloy_ingot"
+  },
+  "toColor": 4565073
+}).id(`kubejs:infuser/allthemodium_unobtainium_vibranium_alloy_ingot`)
+
+//allthemodium-vibranium block
+event.custom({
+  "type": "mythicbotany:infuser",
+  "fromColor": 16750080,
+  "ingredients": [
+    {
+      "item": "allthemodium:allthemodium_block"
+    },
+    {
+      "item": "allthemodium:piglich_heart_block"
+    },
+    {
+     "item": "allthemodium:vibranium_block"
+    }
+  ],
+  "mana": 8100000,
+  "output": {
+    "item": "allthemodium:vibranium_allthemodium_alloy_block"
+  },
+  "toColor": 4565073
+}).id(`kubejs:infuser/allthemodium_vibranium_allthemodium_alloy_block`)
+
+//allthemodium-unobtainium block
+event.custom({
+  "type": "mythicbotany:infuser",
+  "fromColor": 16750080,
+  "ingredients": [
+    {
+      "item": "allthemodium:allthemodium_block"
+    },
+    {
+      "item": "allthemodium:piglich_heart_block"
+    },
+    {
+     "item": "allthemodium:unobtainium_block"
+    }
+  ],
+  "mana": 8100000,
+  "output": {
+    "item": "allthemodium:unobtainium_allthemodium_alloy_block"
+  },
+  "toColor": 6292164
+}).id(`kubejs:infuser/allthemodium_unobtainium_allthemodium_alloy_block`)
+
+//unobtainium-vibranium block
+event.custom({
+  "type": "mythicbotany:infuser",
+  "fromColor": 6292164,
+  "ingredients": [
+    {
+      "item": "allthemodium:unobtainium_block"
+    },
+    {
+      "item": "allthemodium:piglich_heart_block"
+    },
+    {
+     "item": "allthemodium:vibranium_block"
+    }
+  ],
+  "mana": 8100000,
+  "output": {
+    "item": "allthemodium:unobtainium_vibranium_alloy_block"
+  },
+  "toColor": 4565073
+}).id(`kubejs:infuser/allthemodium_unobtainium_vibranium_alloy_block`)
 })
