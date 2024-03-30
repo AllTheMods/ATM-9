cc80406c40a87fce7ac1b18bbd99a4cc4353b70f
Kevin-Marsh <Kevin-Marsh@users.noreply.github.com>
2024-03-31 07:47:25 +1000

    Bug Fixes
    Dye essence recipes for Dyenamics dyes.
    Ae Dense -> Cable backcrafting
    Post Unification fixes

--- kubejs/server_scripts/mods/ae/recipes.js
+++ kubejs/server_scripts/mods/ae/recipes.js
@@ -6,9 +6,7 @@ ServerEvents.recipes(event => {
     E: '#forge:plates/enderium',
     N: 'minecraft:netherite_ingot'
   }).id('kubejs:aeinfinitybooster/infinity_card')
-})
 
-ServerEvents.recipes(event => {
   event.shaped('kubejs:universal_press', ['FPF', 'CSL', 'FEF'], {
     F: '#forge:ingots/sky_steel',
     P: 'ae2:silicon_press',
@@ -17,4 +15,9 @@ ServerEvents.recipes(event => {
     L: 'ae2:logic_processor_press',
     E: 'ae2:engineering_processor_press'
   }).id('kubejs:universal_press')
- })
+ 
+  event.shapeless(` 4x ae2:fluix_covered_cable`,[`ae2:fluix_covered_dense_cable`]).id(`kubejs:ae2/dense_to_normal`)
+  event.shapeless(` 4x ae2:fluix_smart_cable`,[`ae2:fluix_smart_dense_cable`]).id(`kubejs:ae2/smart_dense_to_smart_normal`)
+})
+
+
