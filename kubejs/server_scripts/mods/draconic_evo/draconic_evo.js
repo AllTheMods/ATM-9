860ce3505551cf3cfe9f24e46414f2cb55c762fd
Florian Wilkemeyer <fw@f-ws.de>
2024-08-03 00:34:25 +0200

    Added recipe for converting Draconium Ingot -> Draconium Dust (by Pulverizing, Crushing etc..)  (1:1)
    Normal Way to retreive Draconium dust would be mining Draconium Ore; 1 Dust can be Smelted into one Ingot.
    When using Mystical Agriculture for Resource Generation, Draconium Essence can be crafted into Ingot (which is fine as it's probably the most required resource by DE)
    but theres a few recipes requiring Draconium Dust - before this patch there was no way in retrieving dust when automating resource production with mysitcal agriculture..

--- kubejs/server_scripts/mods/draconic_evo/draconic_evo.js
+++ kubejs/server_scripts/mods/draconic_evo/draconic_evo.js
@@ -40,6 +40,47 @@ ServerEvents.recipes(allthemods =>{
         input: {ingredient: {item: 'draconicevolution:awakened_draconium_ingot'}},
         output: {item: 'draconicevolution:awakened_draconium_dust'}
     }).id(`allthemods:mekanism/crushing/awakened_draconium_dust`)
+
+    allthemods.custom({
+        type: 'create:crushing',
+        ingredients: [{item: 'draconicevolution:draconium_ingot'}],
+        processingTime: 300,
+        results: [
+          {item: 'draconicevolution:draconium_dust'}
+        ]
+    }).id(`allthemods:create/crushing/draconium_dust`)
+
+    allthemods.custom({
+        type: 'thermal:pulverizer',
+        ingredient: {item: 'draconicevolution:draconium_ingot'},
+        result: [
+            {item: 'draconicevolution:draconium_dust'}
+        ],
+        experience: 0.1
+    }).id(`allthemods:thermal/pulverizing/draconium_dust`)
+
+    allthemods.custom({
+        type: "occultism:crushing",
+        ingredient: {item: 'draconicevolution:draconium_ingot'},
+        result: {item: 'draconicevolution:draconium_dust'},
+        crushing_time: 200,
+        ignore_crushing_multiplier: true
+    }).id(`allthemods:occultism/crushing/draconium_dust`)
+
+    allthemods.custom({
+        type: 'immersiveengineering:crusher',
+        energy: 3200,
+        input: {item: 'draconicevolution:draconium_ingot'},
+        result: {count: 1,item: 'draconicevolution:draconium_dust'},
+        secondaries: []
+    }).id(`allthemods:immersive/crushing/draconium_dust`)
+
+    allthemods.custom({
+        type: 'mekanism:crushing',
+        input: {ingredient: {item: 'draconicevolution:draconium_ingot'}},
+        output: {item: 'draconicevolution:draconium_dust'}
+    }).id(`allthemods:mekanism/crushing/draconium_dust`)
+
 })
 
 // This File has been authored by AllTheMods Staff (me, Mitchell52) for use in AllTheMods - AllTheMods 9.
