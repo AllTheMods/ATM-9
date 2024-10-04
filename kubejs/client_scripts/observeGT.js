acc1ccee423bf2177b7b3f1d8fab154b10a6d63f
Offline Online <offlineonline0507@gmail.com>
2024-10-04 09:42:36 +0200

    Update observeGT.js
    Re-Enabling the handler for the "Observe" quests in Gregtech

--- kubejs/client_scripts/observeGT.js
+++ kubejs/client_scripts/observeGT.js
@@ -40,13 +40,13 @@ ClientEvents.tick(allthemods => {
 })
 
 NetworkEvents.dataReceived('customTask', allthemods => {
-    //const {entity, data, level} = event
-    //let taskString = data.task
-    //let task = FTBQuests.getObject(level, taskString)
-    //let playerQuestData = FTBQuests.getData(entity)
-    //if (task && playerQuestData && !playerQuestData.isCompleted(task) && playerQuestData.canStartTasks(task.quest)) {
-    //    playerQuestData.addProgress(task, 1)
-    //}
+    const {entity, data, level} = event
+    let taskString = data.task
+    let task = FTBQuests.getObject(level, taskString)
+    let playerQuestData = FTBQuests.getData(entity)
+    if (task && playerQuestData && !playerQuestData.isCompleted(task) && playerQuestData.canStartTasks(task.quest)) {
+        playerQuestData.addProgress(task, 1)
+    }
 })
 
 // This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 9.
