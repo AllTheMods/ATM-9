f571cb08f3af8bbc8b48a82ea0eae5fe34106dde
WildBr3nex <leighparky4eve@googlemail.com>
2024-10-18 02:19:59 +0100

    Reenabling custom observing quests

--- kubejs/server_scripts/mods/ftbquests/customTask.js
+++ kubejs/server_scripts/mods/ftbquests/customTask.js
@@ -1,7 +1,16 @@
 // This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 9.
 // As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
 
-// ELEMENT REMOVED
+//Renabled Custom Task for observing in quests.
+NetworkEvents.dataReceived('customTask', event => {
+	const {entity, data, level} = event
+    let taskString = data.task
+    let task = FTBQuests.getObject(level, taskString)
+    let playerQuestData = FTBQuests.getData(entity)
+    if (task && playerQuestData && !playerQuestData.isCompleted(task) && playerQuestData.canStartTasks(task.quest)) {
+        playerQuestData.addProgress(task, 1)
+    }
+})
 
 // This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 9.
 // As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
