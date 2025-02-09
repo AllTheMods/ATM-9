cfae6292504fc536cd85035a001990893951793f
Mitchell5200 <mitchell.louth@ozemail.com.au>
2025-02-09 16:14:21 +0800

    1.0.4 stuff 2
    remove broken Croptopia book
    disable more maidens worldgen

--- kubejs/client_scripts/jei_hide.js
+++ kubejs/client_scripts/jei_hide.js
@@ -19,6 +19,7 @@ JEIEvents.hideItems(allthemods => {
     'absentbydesign:stairs_calcite',
     'absentbydesign:slab_calcite'
   ])
+  allthemods.hide('croptopia:guide')
 
   Color.DYE.forEach(color => {
     ['controller', 'creative_controller', 'grid', 'crafting_grid', 'pattern_grid', 'fluid_grid', 'network_receiver', 'network_transmitter', 'relay', 'detector', 'security_manager', 'wireless_transmitter', 'disk_manipulator', 'crafter', 'crafter_manager', 'crafting_monitor'].forEach(machine => {
---
+++ kubejs/server_scripts/mods/croptopia/croptopia.js
@@ -0,0 +1,12 @@
+// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 9.
+// As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
+
+// Temp fix for [https://github.com/ExcessiveAmountsOfZombies/Croptopia/issues/674] where the croptopia book does not work
+
+ServerEvents.recipes(allthemods =>{
+    allthemods.remove({id: 'croptopia:documentation'})
+})
+
+
+// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 9.
+// As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
